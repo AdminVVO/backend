b5753df800bd17f3818c37e073ffa031fd2d255e
AntonioTony <antonio@vivevacationsoutlet.com>
2022-01-25 16:32:52 -0500

    controllercategory

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+class CategoryController extends Controller
+{
+    //
+    public function getCategory(){
+        $category=Category::all();
+        return view('category.CardCategory',compact('serviciotecnicos'));
+    }
+    public function insertCategory(Request $request){
+        $category= Category::create($request->all());
+    }
+    public function updateCategory(Request $request){
+        $category= Category::find();
+        $category->update($request->all());
+    }
+    public function deleteCategory(Request $request){
+        $category= Category::find();
+        $category->delete();
+    }
+}
