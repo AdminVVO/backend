cbd9348f24026e838c3021185f15aebb0f12fed3
AntonioTony <antonio@vivevacationsoutlet.com>
2022-01-28 09:22:07 -0500

    eliminacion de codigo basura y comentarios innecesarios

--- app/Http/Livewire/CategoryTabsItemsSelect.php
+++ app/Http/Livewire/CategoryTabsItemsSelect.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Livewire;
-
 use Livewire\Component;
 use App\Models\Category;
 use Illuminate\Support\Facades\DB;
@@ -10,7 +9,6 @@ class CategoryTabsItemsSelect extends Component
     public function render()
     {
         $category= DB::table('categories')->select('categories.*')->limit(4)->get();
-        // $select=Select::all(); ejemplo de obtener datos para los selects de la vista.
         return view('livewire.category-tabs-items-select',compact('category'));
     }
 }
--- resources/views/livewire/card-category-hotel.blade.php
+++ resources/views/livewire/card-category-hotel.blade.php
@@ -1,5 +1,4 @@
-
-    @foreach ($sites as $item)
+@foreach ($sites as $item)
     <a href="/" class="card_items">
         <div class="card_top">
             <div class="card_top_price">
@@ -13,8 +12,7 @@
         </div>
         <button>
             <div class="card_love">
-                <svg xmlns="http://www.w3.org/2000/svg" width="27.003" height="23.878"
-                    viewBox="0 0 27.003 23.878">
+                <svg xmlns="http://www.w3.org/2000/svg" width="27.003" height="23.878" viewBox="0 0 27.003 23.878">
                     <g transform="translate(1.002 -1.245)" fill="rgba(222,222,222,0.38)" stroke="#dedede"
                         stroke-width="2">
                         <path
@@ -26,30 +24,26 @@
             </div>
         </button>
         <div class="card_img">
-            <img src="{{ URL::asset('assets/img/card/'.$item->thumbNailUrl[0]) }}" alt="">
-            <img src="{{ URL::asset('assets/img/card/'. $item->thumbNailUrl[1]) }}" alt="">
-            <img src="{{ URL::asset('assets/img/card/'. $item->thumbNailUrl[2]) }}" alt="">
+            <img src="{{ URL::asset('assets/img/card/' . $item->thumbNailUrl[0]) }}" alt="">
+            <img src="{{ URL::asset('assets/img/card/' . $item->thumbNailUrl[1]) }}" alt="">
+            <img src="{{ URL::asset('assets/img/card/' . $item->thumbNailUrl[2]) }}" alt="">
         </div>
         <div class="content-dots">
             <span class="dot"></span>
             <span class="dot"></span>
             <span class="dot"></span>
         </div>
-       
+
         <div class="card_info">
             <div class="card_info_text">
                 <h2 class="h2-cards">{{ $item->title }}</h2>
                 <div class="card_info_rating">
                     <i class="fas fa-star"></i>
                     <p>4.89 <span>(15)</span></p>
-                   
+
                 </div>
             </div>
-            
             <h3 class="h3-cards">{{ $item->distance }} miles away</h3>
         </div>
     </a>
-    {{-- solo obtendremos los 2 primeros --}}
-    {{-- estas dos son para las 2 tarjetas que estan debajo de las 9(ya que son mas aplias y de diferente diseno) --}}
 @endforeach
-
