a5a74d135a5a490eb1daaa1307a7dada14e181e9
AntonioTony <antonio@vivevacationsoutlet.com>
2022-01-31 18:23:51 -0500

    migraciones de user

---
+++ database/migrations/2022_01_31_225419_create_pay_methods_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePayMethodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pay_methods', function (Blueprint $table) {
+            $table->id();
+            $table->string('cod');
+            $table->string('card');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pay_methods');
+    }
+}
