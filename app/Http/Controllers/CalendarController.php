a7442bbb452a0a0ddea8b729df74befc1de71059
AlexMarquezD <alex_marquez_davila@hotmail.com>
2022-08-23 11:25:15 -0500

    Validacion de calendar

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -3,18 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Models\Calendar;
+use App\Models\Listing\Listings;
 use Illuminate\Http\Request;
 
 class CalendarController extends Controller
 {
     public function viewCalendar()
     {
+        $listings = Listings::where(auth()->user()->id_user)->get();
+        
+        if(count($listings) == 0){
+            return redirect()->route('/');
+        }
+
         return view('calendar.indexCalendar');
     }
     
     public function viewPersonCalendar()
     {
+        // $this->validateListings();
+        $listings = Listings::where(auth()->user()->id_user)->get();
+        
+        if(count($listings) == 0){
+            return redirect()->route('/');
+        }
+
         return view('calendar.indexPersonCalendar');
     }
-
 }
